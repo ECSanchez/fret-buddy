070e8362d1d214346deb8a48500a016af1383b86
ECSanchez <ecsanchez97@gmail.com>
2020-02-10 23:16:30 -0800

    added basic key change function

--- server.js
+++ server.js
@@ -7,36 +7,7 @@ const db = new sqlite3.Database('.db');
 
 app.use(express.static('static_files'));
 
-app.get('/users', (req, res) => {
-  db.all('SELECT name FROM scales', (err, rows) => {
-    console.log(rows);
-    const allUsernames = rows.map(e => e.name);
-    console.log(allUsernames);
-    res.send(allUsernames);
-  });
-});
 
-app.get('/users/:userid', (req, res) => {
-  const nameToLookup = req.params.userid; // matches ':userid' above
-
-  // db.all() fetches all results from an SQL query into the 'rows' variable:
-  db.all(
-    'SELECT * FROM scales WHERE name=$name',
-    // parameters to SQL query:
-    {
-      $name: nameToLookup
-    },
-    // callback function to run when the query finishes:
-    (err, rows) => {
-      console.log(rows);
-      if (rows.length > 0) {
-        res.send(rows[0]);
-      } else {
-        res.send({}); // failed, so return an empty object instead of undefined
-      }
-    }
-  );
-});
 
 
 // start the server at URL: http://localhost:3000/
--- static_files/functions.js
+++ static_files/functions.js
@@ -1,36 +1,121 @@
-function genFretboard() {
+function genFretboard(iKey) {
     console.log("creating the fretboard");
     // only makes standard tuning guitar
-    var fretboard = document.getElementById("fretboard");
 
-    console.log(fretboard);
+    // create the fretboard
+    // var fretboard = document.getElementById("fretboard");
+    var fretboard = document.createElement("table");
+
+    var key = iKey;
+    var pattern = majorScale;
+    var shiftedPattern = [];
+
+    console.log(majorScale);
+
+    for (var i = 0; i < pattern.length; i++) {
+        shiftedPattern.push( addNotes(pattern[i],key) );
+    }
+    console.log(shiftedPattern);
 
     var sNum;
     var fNum;
 
-    for (sNum = 0; sNum < 6; sNum++) {
+    for (sNum = 5; sNum >= 0; sNum--) {
         // add new row
         var string = document.createElement('tr');
-        for (fNum = 0; fNum < 12; fNum++) {
+        for (fNum = 0; fNum < 15; fNum++) {
             // populate that row
             var fret = document.createElement('td');
-            fret.innerHTML = fNum;
+            var note = addNotes(tuning[sNum],fNum);
+            fret.setAttribute("note", note);
+            fret.innerHTML = noteMap[note];
+
+            if (shiftedPattern.includes(note)) {
+                fret.setAttribute("lit",true);
+            }
+
             string.appendChild(fret);
         }
         fretboard.appendChild(string);
     }
 
+    document.getElementById("fretLocation").appendChild(fretboard);
 
+
+}
+
+function addNotes(a,b) {
+    return (a + b) % 12;
 }
 
 function checkLoad() {
     document.addEventListener('readystatechange', event => {
         if (event.target.readyState === "interactive") {
-            genFretboard();
+            genFretboard(0);
         }
 
     });
 }
 
+
+function keyButton() {
+    // delete old fret board
+    console.log("abandon");
+    const fl = document.getElementById("fretLocation");
+    while(fl.firstChild) {
+        fl.removeChild(fl.firstChild);
+    }
+
+    // get new value
+
+    var e = document.getElementById("keySelect");
+    console.log(e.options[e.selectedIndex].value);
+
+    var key = e.options[e.selectedIndex].value;
+
+
+    genFretboard(noteMap[key]);
+}
+
+// fiddlin
+
+var majorScale = [0,2,4,5,7,9,11];
+var minorScale = [0,2,3,5,7,8,10];
+
+var tuning = [4,9,2,7,11,4]; // standard tuning EADGBE
+
+var noteMap = {     // only flats for now
+    0 : "C" ,
+    1 : "Db",
+    2 : "D" ,
+    3 : "Eb",
+    4 : "E" ,
+    5 : "F" ,
+    6 : "Gb",
+    7 : "G" ,
+    8 : "Ab",
+    9 : "A" ,
+    10: "Bb",
+    11: "B" ,
+    "C" : 0,
+    "Db": 1,
+    "D" : 2,
+    "Eb": 3,
+    "E" : 4,
+    "F" : 5,
+    "Gb": 6,
+    "G" : 7,
+    "Ab": 8,
+    "A" : 9,
+    "Bb": 10,
+    "B" : 11
+}
+
 checkLoad();
+
+//  0  1  2  3  4  5  6  7  8  9  10  11 12  
+//  C     D     E  F     G     A      B  C
+//  1     2     3  4     5     6      7  8
+
+
 
